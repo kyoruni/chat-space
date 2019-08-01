56f9d6c49a927e7ee5bf01521a3625f266e2e58a
miyuki-muneishi <kyoruni@gmail.com>
2019-08-01 15:51:33 +0900

    [Update]groups create action
    groups createアクションを記載

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -5,11 +5,23 @@ class GroupsController < ApplicationController
   end
 
   def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, notice: 'グループを作成しました'
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+  def group_params
+    # user_idの値に配列を渡す
+    params.require(:group).permit(:name, { :user_ids => [] })
   end
 end

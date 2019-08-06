4e4123f5692a18e7ef3593ea33ec82bdd20c5d82
miyuki-muneishi <kyoruni@gmail.com>
2019-08-06 11:23:39 +0900

    [Update] message js
    メッセージを非同期で送信できるようにする

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,25 @@
 $(function() {
+  function buildHTML(message) {
+    var image = message.image ? `<img src= ${message.image}>` : "";
+    var html = `<div class="chat_message">
+                  <div class="chat_message__header">
+                    <p class="chat_message__header__name">
+                      ${message.user_name}
+                    </p>
+                    <p class="chat_message__header__date">
+                    ${message.date}
+                    </p>
+                  </div>
+                  <div class="chat_message__text">
+                    <p>
+                      ${message.body}
+                    </p>
+                    ${image}
+                  </div>
+                </div>`;
+    return html;
+  }
+
   $("#new_message").submit(function(e) {
     e.preventDefault(); // デフォルトのイベント(送信)を止める
 
@@ -12,6 +33,13 @@ $(function() {
       dataType: "json",
       processData: false,
       contentType: false
-    });
+    })
+      .done(function(data) {
+        var html = buildHTML(data);
+        $(".chat_messages").append(html);
+      })
+      .fail(function(data) {
+        alert("エラーが発生したためメッセージは送信できませんでした。");
+      });
   });
 });
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -10,11 +10,21 @@ class MessagesController < ApplicationController
   def create
     @message = @group.messages.new(message_params)
     if @message.save
-      redirect_to group_messages_path, notice: 'メッセージが送信されました'
+      respond_to do |format|
+       format.html { redirect_to group_messages_path, notice: 'メッセージが送信されました' }
+       format.json
+     end
     else
       flash.now[:alert] = 'メッセージを入力してください。'
       render :index
     end
+
+    # if @message.save
+    #   redirect_to group_messages_path, notice: 'メッセージが送信されました'
+    # else
+    #   flash.now[:alert] = 'メッセージを入力してください。'
+    #   render :index
+    # end
   end
 
   private

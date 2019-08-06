b74cf730fb26906ce5156bc3c3c91aa3671a2420
miyuki-muneishi <kyoruni@gmail.com>
2019-08-06 16:42:39 +0900

    [Update]jQuery load
    初回表示時、jQueryが読み込めていなかったので修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,9 @@
 $(function() {
-  function buildHTML(message) {
-    var image = message.image ? `<img src= ${message.image}>` : "";
-    var html = `<div class="chat_message">
+$(document).on("turbolinks:load", function() {
+  $(function() {
+    function buildHTML(message) {
+      var image = message.image ? `<img src= ${message.image}>` : "";
+      var html = `<div class="chat_message">
                   <div class="chat_message__header">
                     <p class="chat_message__header__name">
                       ${message.user_name}
@@ -17,44 +19,45 @@ $(function() {
                     ${image}
                   </div>
                 </div>`;
-    return html;
-  }
+      return html;
+    }
 
-  function scrollBottom(target) {
-    var scrollHeight = $(target)[0].scrollHeight;
-    $(target).animate({ scrollTop: scrollHeight }, 1000);
-  }
+    function scrollBottom(target) {
+      var scrollHeight = $(target)[0].scrollHeight;
+      $(target).animate({ scrollTop: scrollHeight }, 1000);
+    }
 
-  function clearForm() {
-    $("#message_body").val(""); // 入力欄を空にする
-    $('input[type="file"]').val(null); // 選択中の画像を削除する
-  }
+    function clearForm() {
+      $("#message_body").val(""); // 入力欄を空にする
+      $('input[type="file"]').val(null); // 選択中の画像を削除する
+    }
 
-  $("#new_message").submit(function(e) {
-    e.preventDefault();
-    var formData = new FormData(this);
-    var url = $(this).attr("action"); // リクエスト送信先のURLを取得
+    $("#new_message").submit(function(e) {
+      e.preventDefault();
+      var formData = new FormData(this);
+      var url = $(this).attr("action"); // リクエスト送信先のURLを取得
 
-    $.ajax({
-      url: url,
-      type: "POST",
-      data: formData,
-      dataType: "json",
-      processData: false,
-      contentType: false
-    })
-      .done(function(data) {
-        var html = buildHTML(data);
-        var target = ".chat_messages";
-        $(target).append(html);
-        scrollBottom(target);
-        clearForm();
+      $.ajax({
+        url: url,
+        type: "POST",
+        data: formData,
+        dataType: "json",
+        processData: false,
+        contentType: false
       })
-      .fail(function(data) {
-        alert("メッセージを入力してください。");
-      })
-      .always(function(data) {
-        $(".chat_form__message__button").prop("disabled", false); // ボタンを押下可能にする
-      });
+        .done(function(data) {
+          var html = buildHTML(data);
+          var target = ".chat_messages";
+          $(target).append(html);
+          scrollBottom(target);
+          clearForm();
+        })
+        .fail(function(data) {
+          alert("メッセージを入力してください。");
+        })
+        .always(function(data) {
+          $(".chat_form__message__button").prop("disabled", false); // ボタンを押下可能にする
+        });
+    });
   });
 });

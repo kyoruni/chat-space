d4d63ffe853b9ff89cdd43e748ffef2242c83389
miyuki-muneishi <kyoruni@gmail.com>
2019-08-06 10:27:06 +0900

    [Update]message js
    ajaxでフォームのデータを送信できるようにする

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,17 @@
 $(function() {
   $("#new_message").submit(function(e) {
     e.preventDefault(); // デフォルトのイベント(送信)を止める
+
     var formData = new FormData(this);
+    var url = $(this).attr("action"); // リクエスト送信先のURLを取得
+
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: "json",
+      processData: false,
+      contentType: false
+    });
   });
 });

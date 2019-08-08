3ac2f45e414c97e46477cf8fe0596bc5614ba17d
miyuki-muneishi <kyoruni@gmail.com>
2019-08-08 13:47:17 +0900

    [Update]api messages controller
    whereの記述位置を変更

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,10 +1,10 @@
 class Api::MessagesController < ApplicationController
   def index
     @group = Group.find(params[:group_id])
-    @messages = @group.messages.includes(:user)
+    @messages = @group.messages.includes(:user).where('id > ?', params[:id])
     respond_to do |format|
       format.html
-      format.json{ @new_messages = Message.where('id > ?', params[:id]) }
+      format.json
     end
   end
 end

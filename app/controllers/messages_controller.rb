5970be16556e80b00ab5016d4b01be73345753cb
miyuki-muneishi <kyoruni@gmail.com>
2019-08-08 12:13:57 +0900

    [Fix]indent
    インデントが誤っていたので修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -11,9 +11,9 @@ class MessagesController < ApplicationController
     @message = @group.messages.new(message_params)
     if @message.save
       respond_to do |format|
-       format.html
-       format.json
-     end
+        format.html
+        format.json
+      end
     else
       flash.now[:alert] = 'メッセージを入力してください。'
       render :index

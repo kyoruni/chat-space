e33bd32f7106e23920afb5c28a86dfaf147b4457
miyuki-muneishi <kyoruni@gmail.com>
2019-08-08 12:23:59 +0900

    [Update]api messages controller

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,4 +1,10 @@
 class Api::MessagesController < ApplicationController
   def index
+    @group = Group.find(params[:group_id])
+    @messages = @group.messages.includes(:user)
+    respond_to do |format|
+      format.html
+      format.json{ @new_messages = Message.where('id > ?', params[:id]) }
+    end
   end
 end
